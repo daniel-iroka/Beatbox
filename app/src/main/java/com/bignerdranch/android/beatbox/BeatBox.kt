fb3ddf59700bfd62b5fdb40772297a7856476ca5
daniel-iroka <danieliroka67@gmail.com>
2021-12-20 20:14:02 +0100

    Implemented SoundPool to play the sounds for us. It can also do other things like specify how many sounds can play at a time.

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,19 +1,33 @@
 package com.bignerdranch.android.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 
 // This file is responsible for managing our sound assets. This includes finding, keeping track of them and playing the sounds
 // This file is basically us getting the sounds...
 class BeatBox(private val assets: AssetManager) {
     val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder() // creates a SoundPool instance
+        .setMaxStreams(MAX_SOUNDS) // specifies the amount of sounds that can play at a time
+        .build()
 
     init {
         sounds = loadSounds()
+    }
+
+    // this function to play sounds
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
     }
 
     private fun loadSounds(): List<Sound> {
@@ -29,8 +43,21 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach { filename ->
             val assetPath = "$SOUNDS_FOLDER/$filename"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            // Handling the exception for loading sounds in our SoundPool
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException) {
+                Log.e(TAG, "Could not load sound $filename", ioe)
+            }
         }
         return sounds
+    }
+
+    // This function will load sounds into our SoundPool
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
     }
 }
--- app/src/main/java/com/bignerdranch/android/beatbox/Sound.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.kt
@@ -4,7 +4,7 @@ private const val WAV = ".wav"
 
 // This file is responsible for handling information as regards to the sound being currently displayed to the User
 // like keeping track of the fileName of the sound
-class Sound(private val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
 
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
