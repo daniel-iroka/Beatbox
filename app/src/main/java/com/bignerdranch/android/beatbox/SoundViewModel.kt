94e00762c943ccae06f9df8d0ca365caef2faca0
daniel-iroka <danieliroka67@gmail.com>
2021-12-22 22:03:58 +0100

    We used unit tests and Mock objects to test Interaction between our SoundViewModel and BeatBox.playSound(). Buttons successfully playing sounds.

--- app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -6,7 +6,12 @@ import androidx.databinding.Bindable
 // This file(Our MVVMs view model) is responsible for preparing the data our view will display which is a sound and a BeatBox to play that sound
 // and this is what our adapter will directly interact with
 
-class SoundViewModel: BaseObservable() {
+class SoundViewModel(private val beatBox : BeatBox): BaseObservable() {
+    fun onButtonClicked() {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
 
     /**
      *  Note: BaseObservable works like LiveData which are both used to observe changes in data
